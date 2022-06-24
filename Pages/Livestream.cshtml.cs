307019ea18f5c4fd02f3918923fbbbe648bfaa78
Wendy Carpenter <wendy@acmeaom.com>
2022-06-24 17:05:29 -0400

    because this is the inital commit

---
+++ Pages/Livestream.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace swagger_docs.Pages
+{
+    public class LivestreamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
